17f43b3d50659dbd841da81ec691ba366cb803ba
Kobina Otchere <kobinaotchere@Kobinas-MacBook-Pro.local>
2024-08-12 16:10:49 +0000

    Major challenge completed

--- 03_10/script.js
+++ 03_10/script.js
@@ -2,7 +2,7 @@
  * Create a class for the Backpack object type.
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
  */
-import Backpack from "./Backpack.js";
+import { Backpack, HikingBackpack } from "./Backpack.js";
 
 const everydayPack = new Backpack(
   "Everyday Backpack",
--- Practice/03_12/Backpack.js
+++ Practice/03_12/Backpack.js
@@ -37,4 +37,52 @@ class Backpack {
   }
 }
 
-export default Backpack;
+class GymBackpack extends Backpack {
+  constructor(
+    name,
+    volume,
+    color,
+    pocketNum,
+    strapLengthL,
+    strapLengthR,
+    lidOpen,
+    bottleHole,
+    detachableStraps
+  ) {
+    super(name, volume, color, pocketNum, strapLengthL, strapLengthR, lidOpen);
+    this.bottleHole = bottleHole;
+    this.detachableStraps = detachableStraps;
+  }
+  toggleBottleHole(holeStatus) {
+    this.bottleHole = holeStatus;
+  }
+  toggleStraps(strapType) {
+    this.detachableStraps = strapType;
+  }
+}
+
+class HikingBackpack extends Backpack {
+  constructor(
+    name,
+    volume,
+    color,
+    pocketNum,
+    strapLengthL,
+    strapLengthR,
+    lidOpen,
+    hydraPack,
+    pickHolster,
+    gunHarnes
+  ) {
+    super(name, volume, color, pocketNum, strapLengthL, strapLengthR, lidOpen);
+    this.hydraPack = hydraPack;
+    this.pickHolster = pickHolster;
+    this.gunHarnes = gunHarnes;
+  }
+  newStrapLength(lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft + 3;
+    this.strapLength.right = lengthRight - 1;
+  }
+}
+
+export { Backpack, GymBackpack, HikingBackpack };
