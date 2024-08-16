ab56f5a0de0c388551edee4dd62c391c99b3e00a
Kobina Otchere <kobinaotchere@Kobinas-MacBook-Pro.local>
2024-08-16 23:22:10 +0000

    Dom to JS prompting

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -3,6 +3,23 @@
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
 
+import Backpack from "./Backpack.js";
+
+// Functions, objets and modules
+// First Backpack
+const janSport = new Backpack(
+  "JanSport Backpack",
+  35,
+  "grey",
+  "20 days",
+  10,
+  25,
+  25,
+  false
+);
+
+const lidToggler = () => janSport.toggleLid();
+
 // Main content moved to js
 
 const content = `
@@ -25,16 +42,16 @@ const content = `
         <figure class="backpack__image">
             <img src="../../assets/images/everyday.svg" alt="" loading="lazy" />
         </figure>
-        <h1 class="backpack__name">Everyday Backpack</h1>
+        <h1 class="backpack__name">${janSport.name}</h1>
         <ul class="backpack__features">
-            <li class="feature backpack__volume">Volume:<span> 30l</span></li>
-            <li class="feature backpack__color">Color:<span> grey</span></li>
-            <li class="feature backpack__age">Age:<span> 684 days old</span></li>
+            <li class="feature backpack__volume">Volume:<span> ${janSport.volume}</span></li>
+            <li class="feature backpack__color">Color:<span> ${janSport.color}</span></li>
+            <li class="feature backpack__age">Age:<span> ${janSport.age} old</span></li>
             <li class="feature backpack__pockets">
-            Number of pockets:<span> 15</span>
+            Number of pockets:<span> ${janSport.pocketNum}</span>
             </li>
             <li class="feature backpack__strap" data-side="left">
-            Left strap length: <span>26 inches</span>
+            Left strap length: <span>${janSport.strapLen.left} inches</span>
             <form class="leftlength" data-children-count="1">
                 <input
                 type="number"
@@ -45,7 +62,7 @@ const content = `
             </form>
             </li>
             <li class="feature backpack__strap" data-side="right">
-            Right strap length: <span>26 inches</span>
+            Right strap length: <span>${janSport.strapLen.right} inches</span>
             <form class="rightlength" data-children-count="1">
                 <input
                 type="number"
@@ -55,9 +72,11 @@ const content = `
                 <button>Update</button>
             </form>
             </li>
-            <li class="feature backpack__lid">Lid status: <span>closed</span></li>
+            <li class="feature backpack__lid">Lid status: 
+                <span>${janSport.lidStatus}</span>
+            </li>
         </ul>
-        <button class="lid-toggle">Open lid</button>
+        <button id="first-bag-btn" class="lid-toggle" onclick="lidToggler()">Open lid</button>
     </article>
     <article class="backpack" id="pack02">
         <figure class="backpack__image">
@@ -107,3 +126,8 @@ const content = `
 `;
 
 document.body.innerHTML = content;
+// event listeners for lid button
+
+// document
+//   .getElementById("first-bag-btn")
+//   .addEventListener("click", console.log("click"));
